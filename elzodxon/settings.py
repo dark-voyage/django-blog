914bdffc125f364198c27233fa3cdcda13b9a822
Yahyobek Abdunazarov <y.abdunazarov@uicgroup.org>
2022-07-11 20:54:23 +0500

    commit

--- elzodxon/settings.py
+++ elzodxon/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -89,11 +90,29 @@ WSGI_APPLICATION = 'elzodxon.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'blog',
+
+        'USER': 'root',
+
+        'PASSWORD': 'root',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
